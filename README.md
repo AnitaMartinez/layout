ab6d3d4850161d3db49216d4567f537ebc960208
Ana Martínez <amartinezaguilar6@gmail.com>
2019-01-31 20:24:27 +0100

    Added autoprefixer to gulp

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ npm i
 
 #### Run
 
+Open ``` app/index.html``` in the browser.
+
+To modify the project, run:
+
 ```
 gulp sass:watch
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 'use strict';
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-
+const autoprefixer = require('gulp-autoprefixer');
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
-    return gulp.src('./app/scss/*.scss')
+    return gulp.src('./app/styles/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./app/css'));
+        .pipe(gulp.dest('./app/styles/temp'));
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./app/scss/*.scss', gulp.series('sass'));
+gulp.task('sass:watch', function (done) {
+    gulp.watch('./app/styles/scss/**', gulp.series('sass', 'autoprefixer'));
+    done()
 });
 
+gulp.task('autoprefixer', () =>
+    gulp.src('app/styles/temp/index.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/styles/css/'))
+);
